238872b4566cebd553b5d0bfdb11749a7e97e550
Senna Garfio <sennagarfio@gmail.com>
2023-02-24 16:24:54 -0700

    Base CSS for Dev purposes

--- ghi/src/SignupForm.js
+++ ghi/src/SignupForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-// import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import logo from './assets/logo.png'
 
 
 function SignupForm(props) {
@@ -42,51 +43,55 @@ function SignupForm(props) {
 
 	return (
 		<>
-		{/* <div className="form-group-row">
-			<div className="text-center mt-4 name">PanPlan Signup</div> */}
+		<div className="wrapper">
+			<div className="logo">
+			{<img src={logo} height="100"/>}PanPlan
+			</div>
+		<div className="row">
 			<form className="p-3 mt-3" onSubmit={Signup}>
-				<label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
-				{/* <div className="form-field d-flex align-items-center"> */}
-				<div className="form-group row">
-					<input
-					value={username}
-					type="username"
-					onChange={(e) => setUsername(e.target.value)}
-					className="form-control"
-					id="username"
-					aria-describedby="username"
-					placeholder="Enter username"
-					/>
-				</div>
-				<label htmlFor="Password" className="col-sm-2 col-form-label">Password</label>
-				<div className="form-field d-flex align-items-center">
-					<input
-					value={password}
-					type="password"
-					onChange={(e) => setPassword(e.target.value)}
-					className="form-control"
-					id="password"
-					aria-describedby="password"
-					placeholder="Enter Password"
-					/>
-				</div>
-				<label htmlFor="comfirm-password" className="col-sm-2 col-form-label">Confirm Password</label>
-				<div className="form-field d-flex align-items-center">
-					<input
-					value={confirmPassword}
-					type="password"
-					onChange={(e) => setConfirmPassword(e.target.value)}
-					className="form-control"
-					id="confirm-password"
-					aria-describedby="confirm-password"
-					placeholder="Confirm Password"
-					/>
-				</div>
-				<button className="btn mt-3">Sign up</button>
+			<div className="form-field d-flex align-items-center">
+				<span className="***CHANGE***"></span>
+				<input
+				value={username}
+				type="username"
+				onChange={(e) => setUsername(e.target.value)}
+				className="form-control"
+				id="username"
+				aria-describedby="username"
+				placeholder="Enter Username"
+				/>
+			</div>
+			<div className="form-field d-flex align-items-center">
+				<span className="***CHANGE***"></span>
+				<input
+				value={password}
+				type="password"
+				onChange={(e) => setPassword(e.target.value)}
+				className="form-control"
+				id="password"
+				aria-describedby="password"
+				placeholder="Enter Password"
+				/>
+			</div>
+			<div className="form-field d-flex align-items-center">
+				<span className="***CHANGE***"></span>
+				<input
+				value={confirmPassword}
+				type="password"
+				onChange={(e) => setConfirmPassword(e.target.value)}
+				className="form-control"
+				id="confirm-password"
+				aria-describedby="confirm-password"
+				placeholder="Confirm Password"
+				/>
+			</div>
+			<button className="btn mt-3">Sign up</button>
 			</form>
 			<div className="text-center fs-6">
 			<a href="/login">Log in</a>
 			{/* </div> */}
+		</div>
+		</div>
 		</div>
 		</>
 	);
