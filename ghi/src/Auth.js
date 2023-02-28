d5e2aed0b21c5da8c3998c2713046993f6940539
Michelle Xue <michellex0209@gmail.com>
2023-02-28 01:14:37 +0000

    Issue19/add popup window

--- ghi/src/Auth.js
+++ ghi/src/Auth.js
@@ -4,169 +4,170 @@ import { useNavigate } from "react-router-dom";
 let internalToken = null;
 
 export function getToken() {
-  return internalToken;
+    return internalToken;
 }
 
 export async function getTokenInternal() {
-  const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-  try {
-    const response = await fetch(url, {
-      credentials: "include",
-    });
-    if (response.ok) {
-      const data = await response.json();
-      internalToken = data.access_token;
-      return internalToken;
+    const url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/token`;
+    try {
+        const response = await fetch(url, {
+            credentials: "include",
+        });
+        if (response.ok) {
+            const data = await response.json();
+            internalToken = data.access_token;
+            return internalToken;
+        }
     }
-  } catch (e) {}
-  return false;
+    catch (e) { }
+    return false;
 }
 
 function handleErrorMessage(error) {
-  if ("error" in error) {
-    error = error.error;
-    try {
-      error = JSON.parse(error);
-      if ("__all__" in error) {
-        error = error.__all__;
-      }
-    } catch {}
-  }
-  if (Array.isArray(error)) {
-    error = error.join("<br>");
-  } else if (typeof error === "object") {
-    error = Object.entries(error).reduce(
-      (acc, x) => `${acc}<br>${x[0]}: ${x[1]}`,
-      ""
-    );
-  }
-  return error;
+    if ("error" in error) {
+        error = error.error;
+        try {
+            error = JSON.parse(error);
+            if ("__all__" in error) {
+                error = error.__all__;
+            }
+        } catch { }
+    }
+    if (Array.isArray(error)) {
+        error = error.join("<br>");
+    } else if (typeof error === "object") {
+        error = Object.entries(error).reduce(
+            (acc, x) => `${acc}<br>${x[0]}: ${x[1]}`,
+            ""
+        );
+    }
+    return error;
 }
 
 export const AuthContext = createContext({
-  token: null,
-  setToken: () => null,
+    token: null,
+    setToken: () => null,
 });
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+    const [token, setToken] = useState(null);
 
-  return (
-    <AuthContext.Provider value={{ token, setToken }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ token, setToken }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export const useAuthContext = () => useContext(AuthContext);
 
 export function useToken() {
-  const { token, setToken } = useAuthContext();
-  const navigate = useNavigate();
+    const { token, setToken } = useAuthContext();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    async function fetchToken() {
-      const token = await getTokenInternal();
-      setToken(token);
-    }
-    if (!token) {
-      fetchToken();
-    }
-  }, [setToken, token]);
+    useEffect(() => {
+        async function fetchToken() {
+            const token = await getTokenInternal();
+            setToken(token);
+        }
+        if (!token) {
+            fetchToken();
+        }
+    }, [setToken, token]);
 
-  async function logout() {
-    if (token) {
+    async function logout() {
+        if (token) {
+            const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
+            await fetch(url, { method: "delete", credentials: "include" });
+            internalToken = null;
+            setToken(null);
+            navigate("/");
+        }
+    }
+
+    async function login(username, password) {
         const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-      await fetch(url, { method: "delete", credentials: "include" });
-      internalToken = null;
-      setToken(null);
-      navigate("/");
+
+        const form = new FormData();
+        form.append("username", username);
+        form.append("password", password);
+
+        const response = await fetch(url, {
+            method: "post",
+            credentials: "include",
+            body: form,
+        });
+        if (response.ok) {
+            const token = await getTokenInternal();
+            setToken(token);
+            return;
+        }
+        let error = await response.json();
+        return handleErrorMessage(error);
     }
-  }
 
-  async function login(username, password) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-
-    const form = new FormData();
-    form.append("username", username);
-    form.append("password", password);
-
-    const response = await fetch(url, {
-      method: "post",
-      credentials: "include",
-      body: form,
-    });
-    if (response.ok) {
-      const token = await getTokenInternal();
-      setToken(token);
-      return;
+    async function signup(username, password) {
+        const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/sign_up`;
+        const response = await fetch(url, {
+            method: "post",
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (response.ok) {
+            await login(username, password);
+        }
+        return false;
     }
-    let error = await response.json();
-    return handleErrorMessage(error);
-  }
 
-  async function signup(username, password) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/sign_up`;
-    const response = await fetch(url, {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      await login(username, password);
+    async function update(username, password) {
+        const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/accounts`;
+        // const url = `http://localhost:8000/accounts`;
+        const response = await fetch(url, {
+            method: "patch",
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (response.ok) {
+            await login(username, password);
+        }
+        return false;
     }
-    return false;
-  }
 
-  async function update(username, password) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/accounts`;
-    // const url = `http://localhost:8000/accounts`;
-    const response = await fetch(url, {
-      method: "patch",
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      await login(username, password);
-    }
-    return false;
-  }
-
-  return { token, login, logout, signup, update };
+    return { token, login, logout, signup, update };
 }
 
 export const useUser = (token) => {
-  const [user, setUser] = useState();
+    const [user, setUser] = useState();
 
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
 
-    async function get_account() {
-      const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/accounts/{account_id}`;
-      // const url = `http://localhost:8000/current_user`;
-      const response = await fetch(url, {
-        credentials: "include",
-      });
-      if (response.ok) {
-        const newUser = await response.json();
-        setUser(newUser);
-      }
-    }
+        async function get_account() {
+            const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/accounts/{account_id}`;
+            // const url = `http://localhost:8000/current_user`;
+            const response = await fetch(url, {
+                credentials: "include",
+            });
+            if (response.ok) {
+                const newUser = await response.json();
+                setUser(newUser);
+            }
+        }
 
-    get_account();
-  }, [token]);
+        get_account();
+    }, [token]);
 
-  return user;
+    return user;
 }
