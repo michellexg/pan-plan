5a862dbd03b8232f1095f11411f84f9387c5c13c
Senna Garfio <sennagarfio@gmail.com>
2023-03-03 13:31:18 -0700

    Groceries Feature implemented (ugly)

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -9,6 +9,7 @@ import CreateRecipeForm from './common/CreateRecipeForm';
 import DisplayRecipeDetails from './common/RecipeDetails';
 import SignupForm from "./SignupForm.js";
 import MealList from './MealList.js';
+import GroceryList from './Groceries';
 
 function GetToken() {
   useToken();
@@ -44,6 +45,7 @@ function App() {
           <Route path="signup" element={<SignupForm />} />
           <Route path="/" element={<MealList recipes={recipes} />} />
           <Route path="login" element={<LoginForm />} />
+          <Route path="groceries" element={<GroceryList recipes={recipes} />} />
           <Route path="recipes/" element={<RecipeList fetchRecipes={fetchRecipes} recipes={recipes} />} />
           <Route path="recipes/new/" element={<CreateRecipeForm fetchRecipes={fetchRecipes} />} />
           {recipes.map((recipe) => (
