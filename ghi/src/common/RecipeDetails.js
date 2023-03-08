48552c8a8a4b9561fc2b21c43c9556eee4d77638
Michelle Xue <michellex0209@gmail.com>
2023-03-08 11:06:28 -0800

    change some styling

--- ghi/src/common/RecipeDetails.js
+++ ghi/src/common/RecipeDetails.js
@@ -8,19 +8,19 @@ function DisplayRecipeDetails(props) {
   const navigate = useNavigate()
   const [creatorID, setCreatorID] = useState([])
   const fetchToken = async () => {
-      const url = 'http://localhost:8000/token'
-      const fetchConfig = {
-        method: 'get',
-        credentials: 'include',
-      }
-      const response = await fetch(url,fetchConfig)
-      if (response.ok) {
-        const data = await response.json()
-        setCreatorID(data.account.id)
-      }
+    const url = 'http://localhost:8000/token'
+    const fetchConfig = {
+      method: 'get',
+      credentials: 'include',
     }
+    const response = await fetch(url, fetchConfig)
+    if (response.ok) {
+      const data = await response.json()
+      setCreatorID(data.account.id)
+    }
+  }
 
-    useEffect(() => {
+  useEffect(() => {
     fetchToken()
   }, [])
 
@@ -38,40 +38,44 @@ function DisplayRecipeDetails(props) {
       navigate('/recipes')
     }
   }
-    const steps = props.recipe.steps
-    const splitSteps = steps.split('@#$')
-    const ingredients = props.recipe.ingredients
-    const splitIngredients = ingredients.split('@#$')
+  const steps = props.recipe.steps
+  const splitSteps = steps.split('@#$')
+  const ingredients = props.recipe.ingredients
+  const splitIngredients = ingredients.split('@#$')
 
   return (
-    <Card style={{ width: '50%' }}>
-      <Card.Img variant="top" src={props.recipe.image_url} style={{ width: '100%', height: '100%' }} />
-      <Card.Body>
-        <Card.Title>{props.recipe.name}</Card.Title>
-        <Card.Text>
+    <div className='container justify-content-center'>
+      <Card className="recipe-detail-card" style={{ width: '50%' }}>
+        <Card.Img variant="top" src={props.recipe.image_url} style={{ width: '100%', height: '100%' }} />
+        <Card.Body>
+          <Card.Title>{props.recipe.name}</Card.Title>
+          <Card.Text>
             Created by: {props.recipe.creator.username}
             <span>   </span>
             {creatorID === props.recipe.creator.id ?
-                    <button onClick={handleDelete} value={props.recipe.id}>
-                      Delete
-                    </button>
-                  : <span></span>}
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        Ingredients
-        {splitIngredients.map((ingredient) => (
-          <ListGroup.Item key={ingredient}>{ingredient}</ListGroup.Item>
-        ))}
-        Steps
-        {splitSteps.map((step) => (
-          <ListGroup.Item key={step}>{step}</ListGroup.Item>
-        ))}
-      </ListGroup>
-      <Card.Body>
-        <NavLink to="/recipes">Back to recipes</NavLink>
-      </Card.Body>
-    </Card>
+              <button onClick={handleDelete} value={props.recipe.id} className="btn btn-danger">
+                Delete
+              </button>
+              : <span></span>}
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <div className='m-2'>Ingredients</div>
+
+          {splitIngredients.map((ingredient) => (
+            <ListGroup.Item key={ingredient}>{ingredient}</ListGroup.Item>
+          ))}
+          <div className='m-2'>Steps</div>
+
+          {splitSteps.map((step) => (
+            <ListGroup.Item key={step}>{step}</ListGroup.Item>
+          ))}
+        </ListGroup>
+        <Card.Body>
+          <NavLink to="recipes">Back to recipes</NavLink>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
